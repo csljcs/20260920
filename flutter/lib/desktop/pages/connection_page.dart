e4e7b36aaae4b4af1002f5d230fa43a2403073a4
jack <68050336+csljcs@users.noreply.github.com>
2026-09-20 18:54:32 +0800

    Remove Divider and PeerTabPage from connection_page
    updata

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -315,8 +315,7 @@ class _ConnectionPageState extends State<ConnectionPage>
               ],
             ).marginOnly(top: 22),
             SizedBox(height: 12),
-            Divider().paddingOnly(right: 12),
-            Expanded(child: PeerTabPage()),
+
           ],
         ).paddingOnly(left: 12.0)),
         if (!isOutgoingOnly) const Divider(height: 1),
